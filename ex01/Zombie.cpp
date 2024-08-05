70e677f8654689c95bcc8e1c1f8b05cb234b0034
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-08-05 16:09:22 +0700

    Add files via upload

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arodgers <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 12:21:25 by arodgers          #+#    #+#             */
+/*   Updated: 2024/07/27 19:44:32 by arodgers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	Zombie::setName(std::string name)
+{
+	this->_name = name;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie() {} 
+
+Zombie::Zombie(std::string name) : _name(name) {};
+
+Zombie::~Zombie()
+{
+	std::cout << _name << " has perished..." << std::endl;
+}
---
+++ ex01/zombieHorde.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arodgers <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 14:12:53 by arodgers          #+#    #+#             */
+/*   Updated: 2024/07/22 15:38:05 by arodgers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie	*zombieHorde(int N, std::string name)
+{
+	Zombie	*horde;
+
+	horde = new Zombie[N];
+
+	for (int i = 0; i < N; i++)
+		horde[i].setName(name);
+	return (horde);
+}
